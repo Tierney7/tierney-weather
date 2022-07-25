a44e8a985df655b9a505b515c4fb7f10db6a3398
tierney7 <tierneybbq@gmail.com>
2022-07-24 20:25:20 -0500

    first

---
+++ assets/script.js
@@ -0,0 +1,8 @@
+var APIKey = "88a0c91c09104aee7dc546338ab658a0"
+var button = document.querySelector('.button')
+var inputValue = document.querySelector('.inputValue')
+var name = document.querySelector('.name');
+var desc = document.querySelector('.desc');
+var temp = document.querySelector('.temp');
+
+fetch()
