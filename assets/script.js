c927ea3107c5c1979c5c3baf781f0fa422f116cd
tierney7 <tierneybbq@gmail.com>
2022-07-24 20:45:34 -0500

    working

--- assets/script.js
+++ assets/script.js
@@ -5,4 +5,18 @@ var name = document.querySelector('.name');
 var desc = document.querySelector('.desc');
 var temp = document.querySelector('.temp');
 
-fetch()
+button.addEventListener('click',function(){
+    fetch('https://api.openweathermap.org/data/2.5/weather?q='+inputValue.value+'&appid=88a0c91c09104aee7dc546338ab658a0')
+    .then(response => response.json())
+    .then(data => {
+        var nameValue = data['name'];
+        var tempValue = data['main']['temp'];
+        var descValue = data['weather'][0]['description'];
+
+        name.innerHTML = nameValue;
+        temp.innerHTML = tempValue;
+        desc.innerHTML = descValue;
+    })
+
+    .catch(err => alert("Wrong city name!"))
+})
