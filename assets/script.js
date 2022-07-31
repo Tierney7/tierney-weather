2ea9695c66c11dea96f81cc059acde7a162544e5
tierney7 <tierneybbq@gmail.com>
2022-07-30 19:15:33 -0500

    city

--- assets/script.js
+++ assets/script.js
@@ -4,11 +4,16 @@ var inputValue = document.querySelector('.inputValue')
 var name = document.querySelector('.name');
 var desc = document.querySelector('.desc');
 var temp = document.querySelector('.temp');
+var far = document.querySelector('far');
+
+far = temp + 273.15;
+
 
 button.addEventListener('click',function(){
     fetch('https://api.openweathermap.org/data/2.5/weather?q='+inputValue.value+'&appid=88a0c91c09104aee7dc546338ab658a0')
     .then(response => response.json())
     .then(data => {
+        console.log(data)
         var nameValue = data['name'];
         var tempValue = data['main']['temp'];
         var descValue = data['weather'][0]['description'];
