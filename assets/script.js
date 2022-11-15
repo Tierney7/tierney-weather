834a6c7c229b7268bc955ba11b29555be206b71e
Matthew Tierney <tierneybbq@gmail.com>
2022-11-14 23:42:24 -0600

    working finally

--- assets/script.js
+++ assets/script.js
@@ -1,11 +1,11 @@
-var current_q = ""
 var appid = "88a0c91c09104aee7dc546338ab658a0"
+var current_q = ""
 
-var right_col = document.querySelector("#right_col")
-var left_col = document.querySelector("#left_col")
+var leftCol = document.querySelector("#leftCol")
+var rightCol = document.querySelector("#rightCol")
 var units = ""
-var no_result_p = document.querySelector("#not_activated")
 var city_header = document.querySelector("#city_header")
+var no_result_p = document.querySelector("#not_activated")
 var icon = ""
 
 function get_q(element){
@@ -30,21 +30,23 @@ function city_header_section(data){
     return icon, title_img
 }
 
-function right_col_content(data){
-    var res_right = `<div>
-                    <p>High: ${data.main.temp_max}</p>
-                    <p>Low: ${data.main.temp_min} </p>
-                    <p>Feels Like: ${data.main.feels_like}</p>
-                </div>`
-    return res_right
-}
-function left_col_content(data){
+function leftCol_content(data){
     var res_left=   `<div class="weather_content_container">
                         <p>City: ${data.name}</p> 
                         <p>Main: ${data['weather'][0]['main']}</p>
                         <p>Current Temp: ${data.main.temp}</p>        
                     </div>`
     return res_left
+
+}
+
+function rightCol_content(data){
+    var res_right = `<div>
+                    <p>High: ${data.main.temp_max}</p>
+                    <p>Low: ${data.main.temp_min} </p>
+                    <p>Feels Like: ${data.main.feels_like}</p>
+                </div>`
+    return res_right
 
 }
 
@@ -55,8 +57,8 @@ async function search(){
     var weather_data = await response.json()
     console.log(weather_data)
     city_header.innerHTML = city_header_section(weather_data)
-    right_col.innerHTML = right_col_content(weather_data)
-    left_col.innerHTML = left_col_content(weather_data)
+    rightCol.innerHTML = rightCol_content(weather_data)
+    leftCol.innerHTML = leftCol_content(weather_data)
     
 
 }
